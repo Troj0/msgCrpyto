5b06877d3039d25301863a4f6154e5d0c3a00ec6
Al <mu8@tuta.io>
2023-03-01 23:18:22 +0300

    Update msgEncryptRSA.java

--- msgEncryptRSA.java
+++ msgEncryptRSA.java
@@ -6,22 +6,35 @@ import java.security.PublicKey;
 import java.util.Base64;
 import javax.crypto.Cipher;
 
-public class RSAEncryption {
-public static void main(String[] args) throws Exception {
-    
-String plainText = "Hello World!";
-KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-keyGen.initialize(1024);
-KeyPair keyPair = keyGen.genKeyPair();
-PrivateKey privateKey = keyPair.getPrivate();
-PublicKey publicKey = keyPair.getPublic();
-Cipher cipher = Cipher.getInstance("RSA");
-cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
-String encryptedValue = Base64.getEncoder().encodeToString(encryptedBytes);
-System.out.println("Encrypted Value: "+encryptedValue);
 
-}
+public class RSAEncryption {
+        public static void main(String[] args) throws Exception {
+                
+                // Generating pub/pri keys
+                KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+                keyGen.initialize(1024);
+                KeyPair keyPair = keyGen.genKeyPair();
+                PublicKey publicKey = keyPair.getPublic();
+                PrivateKey privateKey = keyPair.getPrivate();
+
+
+                // Encryption
+                String plainText = "Hello World!";
+                Cipher cipher = Cipher.getInstance("RSA");
+                cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+                byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+                String encryptedValue = Base64.getEncoder().encodeToString(encryptedBytes);
+                System.out.println("Encrypted Value: "+encryptedValue+"\n");
+
+
+                // Decryption 
+                cipher.init(Cipher.DECRYPT_MODE, privateKey);
+                byte[] encryptedBytesDecoded = Base64.getDecoder().decode(encryptedValue);
+                byte[] decryptedBytes = cipher.doFinal(encryptedBytesDecoded);
+                String decryptedValue = new String(decryptedBytes);
+                System.out.println("decrypted Value: "+decryptedValue+"\n");
+
+        }
 
 }
 
